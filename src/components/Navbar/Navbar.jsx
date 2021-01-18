4c4903a2b2f44b3c32f396a5d9ee9ad717a3d697
Dan <dantheado@gmail.com>
2021-01-18 21:43:08 +0000

    max-width contatiners

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,29 +17,31 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar-wrapper">
-      <a href="/">
-        <h1>{title || 'siteName'}</h1>
-      </a>
-      <nav>
-        <button className="hamburger-btn" type="button" onClick={toggleHamburger}>
-          <div className="line" />
-          <div className="line" />
-          <div className="line" />
-        </button>
-        <ul className={`nav-links ${hamOpen ? 'open' : ''}`}>
-          {links
-            ? links.map((link) => (
-                <li key={link.id} className={`${hamOpen ? 'fade' : ''}`}>
-                  <a href={link.to} onClick={handleLinkClick}>
-                    {link.name}
-                  </a>
-                </li>
-              ))
-            : ''}
-        </ul>
-      </nav>
-    </div>
+    <section id="navbar-section">
+      <div className="navbar-container">
+        <a href="/">
+          <h1>{title || 'siteName'}</h1>
+        </a>
+        <nav>
+          <button className="hamburger-btn" type="button" onClick={toggleHamburger}>
+            <div className="line" />
+            <div className="line" />
+            <div className="line" />
+          </button>
+          <ul className={`nav-links ${hamOpen ? 'open' : ''}`}>
+            {links
+              ? links.map((link) => (
+                  <li key={link.id} className={`${hamOpen ? 'fade' : ''}`}>
+                    <a href={link.to} onClick={handleLinkClick}>
+                      {link.name}
+                    </a>
+                  </li>
+                ))
+              : ''}
+          </ul>
+        </nav>
+      </div>
+    </section>
   );
 };
 
